d0596c0f785f97413a07b00d04623b9513d77ba8
joonhyuk.han <virq@naver.com>
2021-12-07 23:39:06 +0900

    refactor: integrate functions into class

--- instagram_comment_crawler.py
+++ instagram_comment_crawler.py
@@ -1,148 +1,40 @@
-import selenium
-from selenium import webdriver
-from selenium.webdriver.chrome import options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common import service
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from fake_useragent import UserAgent
-import time
-import random as r
 import getpass
 
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
+from driver import ChromeDriver
+# from RandomPicker import RandomPicker
 
-def set_chrome_driver():
-    user_agent = UserAgent(verify_ssl=False).random
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument(f'user-agent={user_agent}')
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    return driver
-
-# 1 Set Chrome Webdriver (Auto Download)
-driver = set_chrome_driver()
+# 1 Auto Download Chrome Webdriver (Corresponding to Chrome Browser Installed)
+driver = ChromeDriver()
 
 # 2 Move to Instagram Login Page
-def move_to_url(url):
-    return driver.get(url)
-
-login_url = "https://www.instagram.com/accounts/login/"
-raw = move_to_url(login_url)
-driver.implicitly_wait(5)
+driver.move_to_login_page()
 
 # 3 Login Instagram Account
-def login_input_send_keys(username, password):
-    try:
-        types = {'username': username, 'password': password}
-        for type in types:
-            print(type)
-            input_css_selector = f'input[name="{type}"]'
-            inputBox = driver.find_element(By.CSS_SELECTOR, input_css_selector)
-            inputBox.send_keys(types[type])
-            time.sleep(1.5)
-
-        inputBox.send_keys(Keys.RETURN)
-    except ValueError:
-        print("Write Correct ID or Password")
-
-username = getpass.getpass("Input ID : ")       # User ID
+username = input(str("Input ID : "))            # User ID
 password = getpass.getpass("Input Password : ") # User Password
 
-login_input_send_keys(username, password)
+driver.login_to_instagram(username, password)
 
 # 4 Move to Target Post
-# target_url = "https://www.instagram.com/p/B_6ThCKHP4I/"
-# driver.get(target_url)
-
+target_url = "https://www.instagram.com/p/B_6ThCKHP4I/"
+driver.get(target_url)
 
 # 5 Load All Comments
+# Click "comments load button" until all comments have loaded
+driver.load_all_comments()
 
-## Click "comments load button" until comments have all loaded
-# moreCommentLoadBtnCssSelector = "li button.wpO6b"
-# moreCommentLoadBtn = raw.find_element("By.CSS_SELECTOR", moreCommentLoadBtnCssSelector)
-# moreCommentLoadBtn.click()
-# for i in range(999) :
-#     try :
-#         moreCommentLoadBtn = raw.find_element("By.CSS_SELECTOR", moreCommentLoadBtnCssSelector)
-#         moreCommentLoadBtn.click()
-#         driver.implicitly_wait(5)
-#         print("Click Count : ", i + 1)
-#     except :
-#         print("Complete Loading All Comments")
-#         break
+# 6 Load All Replies
+driver.load_all_replies()
 
-# # 4 Load All Replies
-# moreCommentLoadBtn = driver.find_elements("By.CSS_SELECTOR", "div.Igw0E > button.sqdOP.yWX7d,y3zJF")
+# 7 Collect All Comments Writers and Contents
+driver.collect_comments()
 
-# for i in range(999) :
-#     try:
-#         more_box = moreCommentLoadBtn[i].find_element("By.CSS_SELECTOR", "span.EizgU").text
-#         count = int(more_box[-3])
-#         if count >= 9 :
-#             moreCommentLoadBtn[i].click()
-#             time.sleep(0.15)
-#             moreCommentLoadBtn[i].click()
-#             time.sleep(0.15)
-#             moreCommentLoadBtn[i].click()
-#             time.sleep(0.15)
-#         elif count >= 4 :
-#             moreCommentLoadBtn[i].click()
-#             time.sleep(0.15)
-#             moreCommentLoadBtn[i].click()
-#             time.sleep(0.15)
-#         else :
-#             moreCommentLoadBtn[i].click()
-#             time.sleep(0.15)
-#         print("Click Count", i + 1)
-#     except:
-#         print("Complete Loading All Replies")
-#         break
+# 8 Check answer and student ID which is eligible for participate in the event.
+driver.filter_comments()
 
-# # 댓글 더 보기 : div.Igw0E.IwRSH.YBx95._4EzTm.MGdpg button.dCJp8.afkep (1번 누를 때마다 12개씩 추가됨)
-# # 답글 보기 : div.Igw0E > button.sqdOP.yWX7d,y3zJF
-#  ## 답글 갯수 : span.EizgU
-# # 댓글 컨테이너 : ul.Mr508 div.C4VMK
-#  ## 작성자 이름 : a.sqdOP
-#  ## 댓글 내용 : span
+# 9 Pick Random Winners
+# data = driver.answer_list
 
-# # 5 20학번 중 정답자 아이디 수집 (학번 없으면 수집X)
-# driver.implicitly_wait(3)
-# containers = driver.find_elements("By.CSS_SELECTOR", "ul.Mr508 div.C4VMK")
-# comments = driver.find_elements("By.CSS_SELECTOR", "ul.Mr508 div.C4VMK span")
-# award_dic = {}
-
-# ##  댓글 반복 수집
-# id_count = 0
-
-# for i in containers :
-#     comment = i.find_element("By.CSS_SELECTOR", "span")
-#     try :
-#         std_id = comment.text
-#         if "6020" and ("8개" or "8새") in std_id :
-#             account = i.find_element("By.CSS_SELECTOR", "a.sqdOP").text
-#             print(account, "/ 학번 있음 / 정답 O /", std_id)
-#             idx = std_id.index("6020")
-#             id_num = std_id[idx:idx+8]
-#             print("학번", id_num)
-#             id_count += 1
-#             award_dic[account] = id_num
-#     except :
-#         print("오류 발생")
-
-# print("="*20)
-# print("총 댓글 개수 : ", len(comments))
-# print("정답 및 학번 포함 댓글 수 : ", id_count)
-# print("오답 및 학번 없는 댓글 수 : ", len(comments)-id_count)
-# print("="*20)
-# print("정답자 아이디 명단")
-# print(award_dic)
-# print("정답자 : 총",len(award_dic),"명")
-# print("="*20)
-
-# # 7 당첨자 랜덤 추출
-# award = []
 
 # print("치킨 당첨자 : ")
 # while len(award) != 3 :
